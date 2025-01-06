ebece05dd12482c952fae3c676b04211d5378cf9
Kuroto Robert <kuroto@randompolygons.com>
2025-01-06 20:32:22 +0100

    Added a comment in main function.

--- src/main/java/HelloWorld_2.java
+++ src/main/java/HelloWorld_2.java
@@ -2,6 +2,6 @@ public class HelloWorld_2
 {
     public static void main(String[] args)
     {
-
+        System.out.println("This was repo was created in Github then used the URL to add it via IntelliJ.");
     }
 }
